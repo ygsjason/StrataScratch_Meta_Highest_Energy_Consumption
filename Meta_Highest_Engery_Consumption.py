b5fd2e9943c05c1027c81787dbedf9bd2d8ae6bc
ygsjason <99283954+ygsjason@users.noreply.github.com>
2022-02-17 09:17:08 -0600

    Update Meta_Highest_Engery_Consumption.py

--- Meta_Highest_Engery_Consumption.py
+++ Meta_Highest_Engery_Consumption.py
@@ -13,7 +13,7 @@ df_list = [df1, df2, df3]
 df = reduce(lambda left,right: pd.merge(left, right, how = 'outer', on = ['date']), df_list)
 
 # Sum the consumption columns together ignoring na
-df1 = df.assign(total_cspt = lambda df: df[['consumption_x', 'consumption_y', 'consumption']].sum(axis = 1))
+df1 = df.assign(total_cspt = lambda df: df.loc[:,'consumption_x':'consumption'].sum(axis = 1))
 
 # Return the rows where the total_cspt = max values of this column
 df1.sort_values('total_cspt', ascending = [False]).query('total_cspt == total_cspt.max()')
